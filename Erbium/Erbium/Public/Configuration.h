e169e68add5b8cdc26d1c903131583892ed6be7f
Ducki67 <sabel200901@gmail.com>
2025-11-20 21:57:07 +0100

    Update Configuration.h

--- Erbium/Erbium/Public/Configuration.h
+++ Erbium/Erbium/Public/Configuration.h
@@ -3,19 +3,19 @@
 struct FConfiguration
 {
     static inline auto Playlist = L"/Game/Athena/Playlists/Playlist_DefaultSolo.Playlist_DefaultSolo";
-    static inline auto MaxTickRate = 30.f;
+    static inline auto MaxTickRate = 120.f;
     static inline auto bLateGame = false;
-    static inline auto LateGameZone = 3; // starting zone
+    static inline auto LateGameZone = 5; // starting zone
     static inline auto bLateGameLongZone = false; // zone doesnt close for a long time
     static inline auto bEnableCheats = true;
-    static inline auto SiphonAmount = 50; // set to 0 to disable
-    static inline auto bInfiniteMats = false;
-    static inline auto bInfiniteAmmo = false;
+    static inline auto SiphonAmount = 100; // set to 0 to disable
+    static inline auto bInfiniteMats = true;
+    static inline auto bInfiniteAmmo = true;
     static inline auto bForceRespawns = false; // build your client with this too!
     static inline auto bAutoRestart = false;
     static inline auto bKeepInventory = false;
     static inline constexpr auto bEnableIris = true; 
-    static inline constexpr auto bGUI = true;
+    static inline constexpr auto bGUI = false;
     static inline constexpr auto bCustomCrashReporter = true;
     static inline constexpr auto WebhookURL = ""; // fill in if you want status to send to a webhook
 };
